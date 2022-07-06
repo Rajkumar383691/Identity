ecc8879cb4c238e6ec3049e4e2f8ce573a71dc18
Rajkumar383691 <103786632+Rajkumar383691@users.noreply.github.com>
2022-07-06 23:48:12 +0600

    ...

---
+++ rk.py
@@ -0,0 +1,160 @@
+
+
+
+black="\033[1;30m"
+red="\033[0;31m"
+green="\033[0;32m"
+yellow="\033[1;33m"
+blue="\033[1;34m"
+purple="\033[1;35m"
+ciyan="\033[1;36m"
+end="\033[0;0m"
+
+logo= f"""{red}    ██▀███   ██ ▄█▀
+   ▓██ ▒ ██▒ ██▄█▒ 
+   ▓██ ░▄█ ▒▓███▄░ 
+{end}{blue}   ▒██▀▀█▄  ▓██ █▄ 
+   ░██▓ ▒██▒▒██▒ █▄        ▒
+{end}{black}   ░ ▒▓ ░▒▓░▒ ▒▒ ▓▒
+     ░▒ ░ ▒░░ ░▒ ▒░
+     ░░   ░ ░ ░░ ░ 
+      ░     ░  ░   
+                   {end}
+{yellow}.......................
+......{end}{red}RAJ.KUMAR{end}{yellow}........
+.......................{end}"""
+
+
+
+
+import os,time
+
+
+os.system("clear")
+time.sleep(1.1)
+print(logo)
+time.sleep(1.1)
+a=input(purple+"[<] Your Name    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+b=input(purple+"[>] Father's Name    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+c=input(purple+"[>] Mother's Name    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+d=input(purple+"[>] Date of Birth    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+e=input(purple+"[>] Nationality    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+f=input(purple+"[>] Maritial status    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+g=input(purple+"[>] Phone Number    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+h=input(purple+"[>] Email    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+i=input(purple+"[>] Upazilla    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+j=input(purple+"[>] Thana    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+k=input(purple+"[>] Post Office    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+l=input(purple+"[>] Zilla    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+
+
+
+m= input(purple+"SSC Pass in Year    :"+end+ciyan)
+os.system("clear")
+print(logo)
+time.sleep(1.1)
+
+print(red+"Matching........"+end)
+time.sleep(10.1)
+print(red+f"""
+
+
+
+
+
+...Wait For Some Time..."""+end)
+
+time.sleep(10.1)
+os.system("clear")
+print(logo)
+time.sleep(3.1)
+
+
+
+z=yellow+"""
+
+
+...Finally Match Found..."""+end
+print(z)
+time.sleep(1.1)
+print(f"""
+
+
+""")
+
+print(blue+"Name    :"+end,green+ a)
+time.sleep(1.1)
+print(blue+"Father's Name    :"+end,green+ b)
+time.sleep(1.1)
+print(blue+"Mother's Name    :"+end,green+ c)
+time.sleep(1.1)
+print(blue+"Date of Birth    :"+end,green+ d)
+time.sleep(1.1)
+print(blue+"Nationality    :"+end,green+ e)
+time.sleep(1.1)
+print(blue+"Maritial status    :"+end,green+ f)
+time.sleep(1.1)
+print(blue+"Phone Number    :"+end,green+ g)
+time.sleep(1.1)
+print(blue+"Email    :"+end,green+ h)
+time.sleep(1.1)
+print(blue+"Upazilla    :"+end,green+ i)
+time.sleep(1.1)
+print(blue+"Thana    :"+end,green+ j)
+time.sleep(1.1)
+print(blue+"Post Office    :"+end,green+ k)
+time.sleep(1.1)
+print(blue+"Zilla    :"+end,green+ l)
+time.sleep(1.1)
+print(blue+"SSC Pass in Year    :"+end,green+ m)
+time.sleep(3.1)
+
+print(red+f"""
+
+
+            .... END...."""+end)
+
+
+print(ciyan)
+
+
+
+
+
+#Amar Life er 1st Tool😍😍😍
